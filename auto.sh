1495a20b0f0465b900a12e40836cb4193855fe54
cyanpencil <lucadb96@gmail.com>
2020-12-01 17:43:40 +0100

    Some progress on Implementation

--- auto.sh
+++ auto.sh
@@ -1,2 +1,21 @@
-ls *.tex *.sty | entr -s "rm thesis.aux; timeout 5 pdflatex --shell-escape thesis.tex"
+#!/bin/bash
+
+[[ $(echo $* | grep -c ".pdf") -eq 0 ]] && echo "Provide a pdf argument." && exit 1
+[[ ! -f ${1%.pdf}.tex ]] && echo "${1%.pdf}.tex file not found" && exit 1
+
+echo -e "\e[36;1mUse Ctrl+click in Zathura or ,r in vim to sync back and forth!\e[0m"
+
+sleep 0.5
+
+st -e bash -c "vim ${1%pdf}tex --servername $1" &
+
+sleep 0.2
+
+echo "vim --servername $1 --remote +%{line} %{input}"
+
+zathura $1 -x "vim --servername $1 -c \"let g:syncpdf='$1'\" --remote +%{line} %{input}" &
+
+
+ls *.tex *.sty | entr -s "rm thesis.aux; timeout 5 pdflatex -synctex=1 --shell-escape thesis.tex"
+
 #ls *.tex *.sty | entr -s "timeout 10 make"
